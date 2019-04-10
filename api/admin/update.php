c57325cb2970d61f188c03ba60c1ce2cca234719
Git974 <shoaib.ahmad974@gmail.com>
2019-04-10 22:26:35 +0500

    Added admin update file

---
+++ api/admin/update.php
@@ -0,0 +1,21 @@
+<?php
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header('Access-Control-Allow-Methods: PUT');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+  include_once '../../private/admin.class.php';
+
+  $data = json_decode(file_get_contents("php://input"));
+
+  $admin = new Admin();
+
+  $admin->id = $data->id;
+  $admin->firstName = $data->firstName;
+  $admin->lastName = $data->lastName;
+  $admin->username = $data->username;
+  $admin->password = $data->password;
+
+
+  return json_encode($admin->update());
