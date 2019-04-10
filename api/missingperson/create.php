010b907b5c8dd0af2acce92a50c13bbd7bb8ef5d
Git974 <shoaib.ahmad974@gmail.com>
2019-04-10 22:36:01 +0500

    Added missingperson create file

---
+++ api/missingperson/create.php
@@ -0,0 +1,22 @@
+<?php
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header('Access-Control-Allow-Methods: POST');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+  include_once '../../private/missingperson.class.php';
+
+  $data = json_decode(file_get_contents("php://input"));
+
+  $missingPerson = new MissingPerson();
+
+  $missingPerson->firstName = $data->firstName;
+  $missingPerson->lastName = $data->lastName;
+  $missingPerson->age = $data->age;
+  $missingPerson->cnic = $data->cnic;
+  $missingPerson->phone = $data->phone;
+  $missingPerson->address = $data->address;
+  $missingPerson->image = $data->image;
+
+  return json_encode($missingPerson->insert());
