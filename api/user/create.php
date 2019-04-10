49ada2a60a0164d802346c7e6464ca4f958b68f1
Git974 <shoaib.ahmad974@gmail.com>
2019-04-10 22:42:21 +0500

    Added user create file

---
+++ api/user/create.php
@@ -0,0 +1,22 @@
+<?php
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header('Access-Control-Allow-Methods: POST');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+  include_once '../../private/user.class.php';
+
+  $data = json_decode(file_get_contents("php://input"));
+
+  $user = new User();
+
+  $user->firstName = $data->firstName;
+  $user->lastName = $data->lastName;
+  $user->username = $data->username;
+  $user->password = $data->password;
+  $user->email = $data->email;
+  $user->phone = $data->phone;
+  $user->address = $data->address;
+
+  return json_encode($user->insert());
